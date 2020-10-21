ebed59f2b2827518fe73a7f0ce1b192117f55358
Artyom Kilimnik <artyomkilimnik@artyomki-macbook-pro.local>
2020-10-21 19:58:29 +0300

    Updated Day3 and cleaned up the Day3HW files

--- src/com/company/Day3.java
+++ src/com/company/Day3.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Day3 {
+    public static void main(String[] arg) {
+        // Loops
+        // "While" loop
+        System.out.println("'While' loop");
+        int n = 5;
+        while (n > 0) {
+            System.out.println(n);
+            n -= 1;
+        }
+        // "Do ... While" loop
+        System.out.println("'Do .. While' loop");
+        int m = 5;
+        do {
+            System.out.println(m);
+            m -= 1;
+        } while (m > 0);
+
+    }
 }
--- src/com/company/Day3HW.java
+++ src/com/company/Day3HW.java
@@ -1,20 +1,6 @@
 package com.company;
 
 public class Day3HW {
-    public static void main(String [] arg){
-    // Loops
-    // "While" loop
-    int n = 5;
-    while (n>0) {
-        System.out.println(n);
-        n-=1;
-    }
-
+    public static void main(String[] arg) {
     }
 }
-
-
-
-//Local-Online-Marathon
-// 19.10.2020 - 23.10.2020 Online marathon for Frontend and Backend
-//19.10.2020 - 23.10.2020 Online marathon for Frontend and Backend. This repository is crested for the purposes of the MacbookPro/Local PC needs
