0e08e2bc32847bb324544c2de767dc42c933dff3
artyomkilimnik <artyomki@gmail.com>
2020-10-22 20:02:02 +0300

    Updated Da3HW file with the task solution from the Lecture 4.1

--- src/com/company/Day3HW.java
+++ src/com/company/Day3HW.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Day3HW {
     public static void main(String[] arg) {
 
-/*
-
         // Task 3. Написать простой калькулятор:
         //а) пользователь вводит число X
         //б) пользователь вводит число Y
@@ -14,39 +12,47 @@ public class Day3HW {
         //г) программа выводит результат и переходит к шагу (а)
         //д) чтобы программа завершила свою работу нужно ввести пустое значение на шаге (а).
         Scanner sc = new Scanner(System.in);
-        Double x;
         while (true) {
-            System.out.println("Please enter X:");
-            String xCheck = sc.next();
-            if (xCheck.isEmpty()) return;
-                else { x = Double.parseDouble(xCheck);}
+            System.out.print("Please enter X: ");
+            String sX = sc.nextLine();
+            if (sX.isEmpty()) return;
 
-            System.out.println("Please enter Y:");
-            Double y = sc.nextDouble();
+            System.out.print("Please enter Y: ");
+            String sY = sc.nextLine();
 
-            System.out.println("Enter math operation (+, -, *, /, %):");
-            String opr = sc.next(); //why .nextLine() does not work? note: .next() works fine in this case
+            System.out.print("Enter math operation (+, -, *, /, %): ");
+            String sOpr = sc.nextLine();
 
-            if ("+".equals(opr)) {
-                System.out.println(x + opr + y + "=" + (x + y));
-            } else if ("-".equals(opr)) {
-                System.out.println(x + opr + y + "=" + (x - y));
-            } else if ("*".equals(opr)) {
-                System.out.println(x + opr + y + "=" + (x * y));
-            } else if ("/".equals(opr)) {
-                System.out.println(x + opr + y + "=" + (x / y));
-            } else if ("%".equals(opr)) {
-                System.out.println(x + opr + y + "=" + (x % y));
+            double x = Double.parseDouble(sX);
+            double y = Double.parseDouble(sY);
+            double res;
+
+            if ("+".equals(sOpr)) {
+                res = (x + y);
+            } else if ("-".equals(sOpr)) {
+                res = (x - y);
+            } else if ("*".equals(sOpr)) {
+                res = (x * y);
+            } else if ("/".equals(sOpr)) {
+                res = (x / y);
+            } else if ("%".equals(sOpr)) {
+                res = (x % y);
+            } else {
+                System.out.println("Unknown operation!"); break;
             }
+            System.out.println(x + " " + sOpr + " " + y + " " + "= " + res);
             System.out.println("-----------------\r\n");
 
         }
-*/
 
+/*
+
+    // Task 2. Вывести на консоль числа от 1 до 100, которые делятся на 3 без остатка. Например: 3, 6, 9, 12...
         int i;
         for (i = 1; i < 100; i++) {
             if (i % 3 == 0) System.out.println(i);
         }
+*/
 
     }
 }
