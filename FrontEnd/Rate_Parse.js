e7fb3dd8fd9722506d1dc9fcb479de3932f9b4d5
artyomkilimnik <artyomki@gmail.com>
2020-10-23 20:16:49 +0300

    Day 5 HW correction. Not finalized - still 1 issue

--- FrontEnd/Rate_Parse.js
+++ FrontEnd/Rate_Parse.js
@@ -3,9 +3,11 @@ $(document).ready(function ()
   $.getJSON
   ('http://data.fixer.io/api/latest?access_key=0c43401cbf3b9776afa2bd9d30f18934',
   function(data){$('#rate_UAH').text(data.rates.UAH);});
+
   $.getJSON
   ('http://data.fixer.io/api/latest?access_key=0c43401cbf3b9776afa2bd9d30f18934',
   function(data){$('#rate_USD').text(data.rates.USD);});
+
   $.getJSON
   ('http://data.fixer.io/api/latest?access_key=0c43401cbf3b9776afa2bd9d30f18934',
   function(data){$('#date').text(data.date);},);
